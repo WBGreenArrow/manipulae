1ee2f93795f7d070bdba85f6d10ea57e78250d9d
wellison E Brito <wellysone.brito@gmail.com>
2021-10-07 17:58:38 -0300

    Created a proxy to connect from Deezer api

---
+++ manipulae_music_proxy_api/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const axios = require("axios");
+
+const apiPath = "https://api.deezer.com";
+
+function concatQueryParams(query) {
+    let properties = "";
+
+    Object.keys(query).forEach((key) => {
+        if (properties) {
+            properties += `&${key}=${query[key]}`;
+        } else {
+            properties = `?${key}=${query[key]}`;
+        }
+    });
+
+    return properties;
+}
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+app.get("/playlist/:id", (req, res) => {
+    const id = req.params.id;
+
+    const querysParams = req.query;
+
+    const querys = concatQueryParams(querysParams);
+
+    axios.get(`${apiPath}/playlist/${id}${querys}`).then((response) => {
+        res.statusCode = 200;
+
+        return res.json(response.data);
+    });
+});
+
+app.listen(3001);
